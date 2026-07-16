2cd390010197f373baf58578f5d37fd6e666ffa6
RabeetOffice <120552370+RabeetOffice@users.noreply.github.com>
2026-07-16 11:55:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -947,7 +947,7 @@ if ($page == 'index') {
 
             <div class="testimonial-video">
                 <iframe
-                    src="https://www.youtube.com/embed/_LNYIu8Odf4?rel=0"
+                    src="https://www.youtube.com/embed/0fky3jcVTRg?rel=0"
                     title="Client video testimonial"
                     loading="lazy"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
